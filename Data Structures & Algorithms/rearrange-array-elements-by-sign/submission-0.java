85c577619cbbd90da061b4ecf2c3cfc3ad3384d6
Rahamatulla <khaja.rahamatulla44@gmail.com>
2026-08-12 23:12:27 +0530

    Add: rearrange-array-elements-by-sign - submission-0

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        for(int n:nums){
+            if(n>=0){
+                pos.add(n);
+            }
+            else{
+                neg.add(n);
+            }
+        } // [3,1,2] [-2,-5,-4]
+        int l=0;
+        int k=0;
+        int i=0;
+        while(l<pos.size() && k<neg.size()){
+            if(l<pos.size()){
+                nums[i]=pos.get(l);
+                i++;
+            }
+            if(k<neg.size()){
+                nums[i]=neg.get(k);
+                i++;
+            }
+            l++;
+            k++;
+        }
+        return nums;
+    }
+}
